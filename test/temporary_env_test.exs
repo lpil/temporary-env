7a72511938890dc73875d78a3b0a1f1a9d84473a
Louis Pilfold <louis@lpil.uk>
2015-10-18 15:34:09 +0100

    Test when an error is raised

--- test/temporary_env_test.exs
+++ test/temporary_env_test.exs
@@ -23,4 +23,28 @@ defmodule TemporaryEnvTest do
     end
     assert Application.get_env(:tmp_test, :baz, :some_default) == :some_default
   end
+
+  test "resets an already set value when an exception is raised" do
+    try do
+      Application.put_env(:tmp_test, :bar, "hello")
+      TemporaryEnv.set :tmp_test, bar: 2 do
+        raise SyntaxError
+      end
+    rescue
+      SyntaxError -> nil
+    end
+    assert Application.get_env(:tmp_test, :bar) == "hello"
+  end
+
+  test "resets an unset value when an exception is raised" do
+    try do
+      Application.delete_env(:tmp_test, :baz)
+      TemporaryEnv.set :tmp_test, baz: 3 do
+        raise SyntaxError
+      end
+    rescue
+      SyntaxError -> nil
+    end
+    assert Application.get_env(:tmp_test, :baz, :some_default) == :some_default
+  end
 end
